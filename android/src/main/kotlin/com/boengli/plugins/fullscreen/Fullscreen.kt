0da03cfb722c0710a9aa4754472d7eca8423183c
Richard Lutz <r.lutz@cloudtec.ch>
2024-11-04 17:05:20 +0100

    Update

--- android/src/main/kotlin/com/boengli/plugins/fullscreen/Fullscreen.kt
+++ android/src/main/kotlin/com/boengli/plugins/fullscreen/Fullscreen.kt
@@ -4,7 +4,9 @@ import android.os.Build
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.getcapacitor.Plugin
 import com.getcapacitor.PluginCall
@@ -18,9 +20,11 @@ class Fullscreen : Plugin() {
     fun activateImmersiveMode(call: PluginCall) {
         val activity = bridge.activity
         if (activity != null && isImmersiveModeSupported()) {
-            hideSystemBars(activity)
-            setupVisibilityListeners(activity)
-            call.resolve()
+            activity.runOnUiThread {
+                hideSystemBars(activity)
+                setupVisibilityListeners(activity)
+                call.resolve()
+            }
         } else {
             call.reject("Immersive mode is not supported on this device.")
         }
@@ -30,8 +34,10 @@ class Fullscreen : Plugin() {
     fun deactivateImmersiveMode(call: PluginCall) {
         val activity = bridge.activity
         if (activity != null && isImmersiveModeSupported()) {
-            showSystemBars(activity)
-            call.resolve()
+            activity.runOnUiThread {
+                showSystemBars(activity)
+                call.resolve()
+            }
         } else {
             call.reject("Cannot deactivate immersive mode.")
         }
@@ -43,64 +49,44 @@ class Fullscreen : Plugin() {
 
     private fun hideSystemBars(activity: android.app.Activity) {
         val window = activity.window
+        val decorView = window.decorView
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // For Android 11 (API level 30) and above
-            WindowCompat.setDecorFitsSystemWindows(window, false)
-            val controller = window.insetsController
-            controller?.let {
-                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-        } else {
-            // For Android versions below 11
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-            )
-        }
+        // Ensure that we do not fit system windows
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Use WindowInsetsControllerCompat for better compatibility
+        val controller = WindowInsetsControllerCompat(window, decorView)
+
+        // Hide the system bars
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
     private fun showSystemBars(activity: android.app.Activity) {
         val window = activity.window
+        val decorView = window.decorView
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // For Android 11 (API level 30) and above
-            WindowCompat.setDecorFitsSystemWindows(window, true)
-            val controller = window.insetsController
-            controller?.show(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-        } else {
-            // For Android versions below 11
-            @Suppress("DEPRECATION")
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-        }
+        // Reset to fit system windows
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
+        val controller = WindowInsetsControllerCompat(window, decorView)
+
+        // Show the system bars
+        controller.show(WindowInsetsCompat.Type.systemBars())
     }
 
     private fun setupVisibilityListeners(activity: android.app.Activity) {
         val decorView = activity.window.decorView
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            // For Android 11 and above
-            decorView.setOnApplyWindowInsetsListener { _, insets ->
-                val isVisible = insets.isVisible(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
-                if (isVisible) {
-                    hideSystemBars(activity)
-                }
-                insets
-            }
-        } else {
-            // For Android versions below 11
-            @Suppress("DEPRECATION")
-            decorView.setOnSystemUiVisibilityChangeListener { visibility ->
-                if ((visibility and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+        // Listener to re-hide system bars when they reappear
+        ViewCompat.setOnApplyWindowInsetsListener(decorView) { _, insets ->
+            val isVisible = insets.isVisible(WindowInsetsCompat.Type.systemBars())
+            if (isVisible) {
+                activity.runOnUiThread {
                     hideSystemBars(activity)
                 }
             }
+            insets
         }
     }
 }
